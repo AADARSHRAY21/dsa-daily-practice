a74cf4cda8853e0cfd38886becd92dcc52e6ec24
AADARSHRAY21 <21raiaadarsh@gmail.com>
2026-07-09 14:38:34 +0530

    Day 2: Solved - Longest Common prefix (//    for(int i =0; i<strs.length ; i++) {  //Compare with remaining String)

---
+++ arrays/longest-common-prefix.java
@@ -0,0 +1,19 @@
+/*
+ * Problem: Longest Common prefix (LeetCode #14)
+ * Link: class Solution {
+ * Difficulty: //    if(strs == null || strs.length ==0) return "";
+ * Pattern: //    for(int i =0; i<strs.length ; i++) {  //Compare with remaining String
+ *
+ * Approach (in my own words):
+ * -
+ *
+ * Time Complexity: O(?)
+ * Space Complexity: O(?)
+ *
+ * Needed hint? //         prefix = prefix.substring(0, prefix.length() -1);  //Remove Last Character
+ *
+ * Revisit on: (7 days from 2026-07-09)
+ */
+
+// TODO: paste your solved code below
+
