5a88213837e8d3aa5ba4f6cb938b66065ae821d0
AADARSHRAY21 <21raiaadarsh@gmail.com>
2026-08-23 20:55:39 +0530

    remove duplicate questions from array.

--- arrays/contains-duplicate.java
+++ arrays/contains-duplicate.java
@@ -1,18 +1,18 @@
 /*
- * Problem: Contains Duplicate (LeetCode #217)
+ * Problem: Contain Duplicate (LeetCode #217)
  * Link: https://leetcode.com/problems/contains-duplicate/
  * Difficulty: Easy
- * Pattern: Hashing(HashSte, HashMap)
+ * Pattern: Hahsing(HashSet,HashMap)
  * Topics: arrays
  *
  * Approach (in my own words):
  * -
  *
- * Time Complexity: O(n) average — each element is processed once, and HashSet.add() is O(1) average.
- * Space Complexity: O(n) — in the worst case, all elements are unique and stored in the set.
+ * Time Complexity: : O(n) average — each element is processed once, and HashSet.add() is O(1) average.
+ * Space Complexity: : O(n) — in the worst case, all elements are unique and stored in the set.
  *
  * Needed hint? yes
- * Notes : 1.HashSet stores unique elements only. 2.add() directly helps detect duplicates because it returns false if the element already exists. 3.Early exit: As soon as a duplicate is found, return true. 4No need to use contains() and then add() separately.
+ * Notes : 1.Use a HashSet to track seen elements. If add() returns false, the element already exists, so return true. 2.HashSet stores unique elements only. 3.add() directly helps detect duplicates because it returns false if the element already exists. 4.Early exit: As soon as a duplicate is found, return true. 5.No need to use contains() and then add() separately.
  * Revisit on: (7 days from 2026-07-18)
  */
 
