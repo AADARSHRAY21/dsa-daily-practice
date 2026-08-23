c9953de1ae19f25592fd835ab5211e30f64a7642
AADARSHRAY21 <21raiaadarsh@gmail.com>
2026-08-23 20:00:21 +0530

    Update moves zero(array, two-pointers) code.

--- arrays/move-zeroes.java
+++ arrays/move-zeroes.java
@@ -16,5 +16,32 @@
  * Revisit on: (7 days from 2026-08-23)
  */
 
-// TODO: paste your solved code below
+class Solution {
+    public void moveZeroes(int[] nums) {
+    int index =0;
+    for(int i : nums) {
+        if( i != 0) {
+        nums[index] = i;
+        index++;
+        }
+    }
+    while( index < nums.length) {
+        nums[index] = 0;
+        index++;
+    }
+
+    //or
+
+    // int index =0;
+    // for(int i =0; i < nums.length; i++) {
+    //  if(nums[i] != 0) {
+    //     int temp = nums[i];
+    //     nums[i] = nums[index];
+    //     nums[index] = temp;
+    //     index++;
+    //  }
+    // }
+
+    }
+}
 
